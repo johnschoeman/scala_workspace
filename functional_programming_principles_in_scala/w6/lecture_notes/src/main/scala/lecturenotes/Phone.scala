429d4dc17b305428ace7b75f4933678b00047ccd
John Schoeman <johnschoeman1617@gmail.com>
2019-07-12 17:05:09 -0400

    Refactor 6.5

--- functional_programming_principles_in_scala/w6/lecture_notes/src/main/scala/lecturenotes/Phone.scala
+++ functional_programming_principles_in_scala/w6/lecture_notes/src/main/scala/lecturenotes/Phone.scala
@@ -1,7 +1,7 @@
 package phone
 
 object Phone {
-  val words = List("SCALA", "IS", "FUN", "DVO", "I", "H", "LIKE", "CHEESE")
+  val words = List("scala", "is", "fun", "dvo", "I", "H", "like", "cheese")
 
   val mnem = Map(
     '2' -> "ABC",
@@ -14,46 +14,38 @@ object Phone {
     '9' -> "WXYZ"
     )
 
-    val charCode: Map[Char, Char] = Map(
-      'A' -> '2', 'B' -> '2', 'C' -> '2',
-      'D' -> '3', 'E' -> '3', 'F' -> '3',
-      'G' -> '4', 'H' -> '4', 'I' -> '4',
-      'J' -> '5', 'K' -> '5', 'L' -> '5',
-      'M' -> '6', 'N' -> '6', 'O' -> '6',
-      'P' -> '7', 'Q' -> '7', 'R' -> '7',
-      'S' -> '7',
-      'T' -> '8', 'U' -> '8', 'V' -> '8',
-      'W' -> '9', 'X' -> '9', 'Y' -> '9',
-      'Z' -> '9'
-    )
-
-  def translate(phoneNumber: String) = {
-
-    def translateNext(number: String): Set[List[String]] = {
-      if (number.length == 0) {
-        Set(List())
-      }
-      else {
-        for {
-          index <- (1 to number.length).toSet map((i: Int) => List(i.toString))
-          words <- translateNext(number drop index.head.toInt)
-          val subNumber = number take index.head.toInt
-          potentialWord <- matchingWords(subNumber)
-        } yield {
-          potentialWord :: words
-        }
-      }
-    }
-
-    translateNext(phoneNumber)
+  val charCode: Map[Char, Char] = {
+    for {
+      (digit, str) <- mnem
+      ltr <- str
+    } yield (ltr, digit)
   }
 
   def wordCode(word: String): String = {
-    word map(char => charCode(char))
+    word.toUpperCase map(char => charCode(char))
   }
 
-  def matchingWords(number: String): List[String] = {
-    words filter(word => wordCode(word) == number)
+  val matchingWords: Map[String, Seq[String]] = {
+    (words groupBy wordCode).withDefaultValue(Seq())
+  }
+
+  def translate(phoneNumber: String) = {
+    def encode(number: String): Set[List[String]] = {
+      if (number.isEmpty) Set(List())
+      else {
+        {
+          for {
+            index <- (1 to number.length)
+            words <- encode(number drop index)
+            potentialWord <- matchingWords(number take index)
+          } yield {
+            potentialWord :: words
+          }
+        }.toSet
+      }
+    }
+
+    encode(phoneNumber).map(_.mkString(" "))
   }
 
   println(translate("7225247386"))
